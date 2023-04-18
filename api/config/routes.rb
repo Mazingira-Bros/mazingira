06da38f6abb3501878fd1bff9f8c27ea3995cb95
Stiflerzak <hilarymwendia97@gmail.com>
2023-04-18 15:22:16 +0300

    Created destroy method

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :beneficiaries
   resources :stories
   resources :transactions
-  resources :reminders
+  resources :reminders 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
