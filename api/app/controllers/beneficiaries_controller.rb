21e454b1912b5430293511b1a390e3a7f7b45854
stephen-nene <stephen.nene@student.moringaschool.com>
2023-04-18 12:40:12 +0300

    at beneficiary

--- api/app/controllers/beneficiaries_controller.rb
+++ api/app/controllers/beneficiaries_controller.rb
@@ -1,6 +1,34 @@
 class BeneficiariesController < ApplicationController
+  before_action :set_beneficiary, only: [:show, :update, :destroy]
+
+  #GET /beneficiaries
   def index
     @beneficiaries = Beneficiary.all
-    render json: @beneficiaries
+    render json: @beneficiaries, status: :ok
   end
+
+  #GET /beneficiaries/:id
+  def show
+    render json: @beneficiary, status: :ok
+  end
+
+  #POST /beneficiaries
+  #PATCH /beneficiaries/:id
+  #DELETE /beneficiaries/:id
+  def destroy
+    @beneficiary.destroy
+  end
+
+
+  private
+  #set callback
+  def set_beneficiary
+    @beneficiary =Beneficiary.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def beneficiary_params
+    params.require(:beneficiary).permit(:organisation_id, :name, :image, :description, :location)
+  end
+
 end
