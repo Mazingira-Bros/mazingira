4494d4aa9c40db315862d52a11d38e3223971a6a
joseph mwanzia <makamwanzia@gmail.com>
2023-04-18 14:11:49 +0300

    Donor controllers

--- api/app/controllers/donors_controller.rb
+++ api/app/controllers/donors_controller.rb
@@ -1,6 +1,32 @@
 class DonorsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_donor_not_found_response
+
+  # GET /donors
   def index
-    @donors = Donor.all
-    render json: @donors
+    donors = Donor.all
+    render json: donors, status: :ok
+  end
+
+  # SHOW /donors/:id
+  def show 
+    donor = Donor.find(params[:id])
+    render json: donor, status: :ok
+  end
+
+  # PUT /donor/update/:id
+  def update 
+    donor = Donor.find(params[:id])
+    donor.update(donor_params)
+    render json: donor, status: :created
+  end
+
+  private
+
+  def donor_params
+    params.permit(:name, :email, :password_digest)
+  end
+
+  def render_donor_not_found_response
+    render json: {error: "Donor not found"}, status: :not_found
   end
 end
---
+++ api/app/serializers/donor_serializer.rb
@@ -0,0 +1,3 @@
+class DonorSerializer < ActiveModel::Serializer
+  attributes :id, :name, :email, :password_digest
+end
