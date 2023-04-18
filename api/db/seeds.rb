8dc96c90441e836d81d18ad182a6019f19a329fa
joseph mwanzia <makamwanzia@gmail.com>
2023-04-18 10:48:08 +0300

    Minor changes

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts "🌱 creating reminders..."
+
+transactions = Transaction.create([{donation_id:"#{donations.id}",payment_method:"M-pesa",status:"Pending"}])
+
+
+puts "✅Done"
