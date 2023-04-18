2659045dfe47d5978f215fea718b200fb1b3a757
treva556 <marktreva92@gmail.com>
2023-04-18 11:56:27 +0300

    added image & icon for landing page

---
+++ client/src/App.test.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect"
+import { render } from "@testing-library/react";
+import App from "../App.jsx";
+
+
+test("renders content", ()=>{
+    const component = render(<App></App>)
+    console.log(component)
+})
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-         <h1 className="text-3xl font-bold underline bg-slate-500">
+         <h1 className="text-2xl font-bold underline bg-slate-500">
       Hello world!
     </h1>
     </div>
