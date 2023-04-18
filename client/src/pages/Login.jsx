432268079125ec31b1a554d2a643d16b7da26482
certihimo <kalidahm3d@gmail.com>
2023-04-18 14:30:32 +0300

    done

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = { username, password };
+    fetch('http://127.0.0.1:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  };
+
+  return (
+    <div className='login-cont'>
+    <form onSubmit={handleSubmit} className="login-form">
+      <h2>Login</h2>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+      <br />
+      <div className="links">
+        <p>Forgot Password?</p>
+        <Link to={'/signup'}>Signup</Link>
+      </div>
+    </form>
+    </div>
+  );
+}
+
+export default Login;
