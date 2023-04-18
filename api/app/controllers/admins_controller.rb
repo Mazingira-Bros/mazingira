5fa853f75996278f5481b4ff71761a55760117bf
Stiflerzak <hilarymwendia97@gmail.com>
2023-04-18 15:31:48 +0300

    Created admin controller method

---
+++ api/app/controllers/admins_controller.rb
@@ -0,0 +1,6 @@
+class AdminsController < ApplicationController
+    def index
+    admins= Admin.all
+    render json: admins
+    end
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :stories
   resources :transactions
   resources :reminders 
+  resources :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
