cd8ebc686a3c56d703f00f7dc68b93083de3ed91
Stiflerzak <hilarymwendia97@gmail.com>
2023-04-18 11:39:19 +0300

    Created method show

--- api/app/controllers/transactions_controller.rb
+++ api/app/controllers/transactions_controller.rb
@@ -1,6 +1,21 @@
 class TransactionsController < ApplicationController
+    # displays all transactions
     def index
     transactions= Transaction.all 
-    render json: transactions
+    render json: transactions, status: :ok
     end
+
+    # This shows a specific Transaction
+    def show
+        transaction = Transaction.find_by(id: params[:id])
+
+    if transaction
+      render json: transaction
+    else
+      render json: { error: "Transaction not found" }, status: :not_found
+    end
+  end
+
+  
+
 end
