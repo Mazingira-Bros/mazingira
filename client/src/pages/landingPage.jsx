2bb818c0054daa756fe02e11b8d8d159fec986ce
treva556 <marktreva92@gmail.com>
2023-04-19 01:49:41 +0300

    landing page

--- client/src/pages/landingPage.jsx
+++ client/src/pages/landingPage.jsx
@@ -1,57 +1,26 @@
 import React from 'react'
 
 import image from '../utilities/images/image.jpg'
-
+import icon from '../utilities/icons/title-icon.png'
 
 function LandingPage() {
   return (
     <div className='flexxie'>
-        <div className={styles['dummy-container']}>
-        <span className={styles['text']}>
-          <span>hhhhhhhhhhhhhhhhhh</span>
-        </span>
-      </div>
-   <div>
+    < div className='container'>
+      <div className='item-1'>
+        <img src={image} >
 
-      <div className={styles['dummy-container']}>
-        <span className={styles['text']}>
-          <span>Log In</span>
-        </span>
+        </img>
+      </div>
+      <div className='item-2'>
+        {/* <img src={icom}></img> */}
+        <br></br>
+        <p>  We Have The Power Today To Change Tomorrow!  </p>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">
+  Get Started
+</button>
       </div>
     </div>
-    <div className={styles['dummy-container']}>
-        <span className={styles['text']}>
-          <span>MAZINGIRA</span>
-        </span>
-      </div>
-      <img
-        src="/playground_assets/rectangle111872-2qq5-1700w.png"
-        alt="Rectangle111872"
-        className={styles['rectangle111']}
-      />
-      <div className={styles['dummy-container']}></div>
-      <div className={styles['dummy-container']}>
-        <img
-          src="/playground_assets/rectangle112873-szj-1000h.png"
-          alt="Rectangle112873"
-          className={styles['rectangle112']}
-        />
-      </div>
-      <div className={styles['dummy-container']}>
-        <img
-          src="/playground_assets/rectangle113876-1wg3-200h.png"
-          alt="Rectangle113876"
-          className={styles['rectangle113']}
-        />
-      </div>
-      <div className={styles['dummy-container']}>
-        <img
-          src="/playground_assets/titleicon5875-stdi-200h.png"
-          alt="titleicon5875"
-          className={styles['titleicon5']}
-        />
-      </div>
-
  </div>
 
   )
