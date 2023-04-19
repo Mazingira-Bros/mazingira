84db7ece3c759dea5309ba93d16993d72540b4ab
joseph mwanzia <makamwanzia@gmail.com>
2023-04-19 19:13:19 +0300

    UseState

--- client/src/components/Admin/RegistrationForm.js
+++ client/src/components/Admin/RegistrationForm.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useState } from 'react'
 
 function RegistrationForm() {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+  const [errors, setErrors] = useState({})
+
+  
+
+
   return (
     <div>RegistrationForm</div>
   )
