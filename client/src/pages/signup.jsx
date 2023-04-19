de5a19fef329db75960ddd66cfdee5b7156895fd
certihimo <kalidahm3d@gmail.com>
2023-04-19 09:59:00 +0300

    done

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -18,3 +18,27 @@ function Signup() {
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = { username, email, password};
+    fetch('http://127.0.0.1:3000/signup',{
+        method:'POST',
+        header: {
+            'content-Type': 'application/json',
+        },
+        body:JSON.stringyfy(data),
+    })
+        .then((response) =>{
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+        })
+        .then((data) =>{
+            console.log(data);
+        })
+    .   .catch((error) => {
+        
+    })
+  }
