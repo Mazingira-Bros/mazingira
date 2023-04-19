a947b3d55a1b59345aea0b2f0b956b980a618cd8
joseph mwanzia <makamwanzia@gmail.com>
2023-04-19 20:13:37 +0300

    Handlesubmit event

--- client/src/components/Admin/RegistrationForm.js
+++ client/src/components/Admin/RegistrationForm.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import { useState } from 'react'
+import Login from '../Login'
 
 function RegistrationForm() {
-  const [username, setUsername] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [passwordConfirmation, setPasswordConfirmation] = useState('')
-  const [errors, setErrors] = useState({})
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [errors, setErrors] = useState({});
 
-  
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const userData = { name, email, password, passwordConfirmation: passwordConfirmation };
+  fetch("/admin", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(userData)
+  }, [])
+  .then(res =>{
+    if (res.ok) {
+      return <Login />
+    } else {
+      return <RegistrationForm />
+      response.json().then(data => setErrors(data.errors));
+    }
+  }):
+}
+
 
 
   return (
