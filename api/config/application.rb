cb2d9aaf3dca28858a1a4d2c0919f45bffd3922a
stephen-nene <stephen.nene@student.moringaschool.com>
2023-04-24 16:17:10 +0300

    done with login and logout

--- api/config/application.rb
+++ api/config/application.rb
@@ -18,12 +18,14 @@ module MazingiraProject
       end
     end
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.api_only = false
+
+    # Must add these lines!
+    # Adding back cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Use SameSite=Strict for all cookies to help protect against CSRF
+    config.action_dispatch.cookies_same_site_protection = :strict
   end
 end
